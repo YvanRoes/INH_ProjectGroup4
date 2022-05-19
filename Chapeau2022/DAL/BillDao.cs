04f7f55dd95bde20d94c62047bcc59810575da57
MTKabir <658116@student.inholland.nl>
2022-05-19 14:51:46 +0200

    Bill model, Logic ,Dao and form Created

---
+++ Chapeau2022/DAL/BillDao.cs
@@ -0,0 +1,22 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class BillDao : BaseDao
+    {
+        //Inser the  bill into the database
+        public void BillRecord(Bill bill)
+        {
+            string query = $"INSERT INTO [BILL] VALUES ({bill.Method}, '{bill.Tip}', '{bill.Price}', '{bill.DateTime.ToString("dddd, dd MMMM yyyy")}', {bill.EmployeeID}, {bill.OrderNr})";
+            ExecuteEditQuery(query, new SqlParameter[0]);
+        }
+
+
+    }
+}
