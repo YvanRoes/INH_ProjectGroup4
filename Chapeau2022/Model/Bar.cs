0691fe68b583a0cc35db84850324b8961e5f4ea5
Jun Feng <678234@student.inholland.nl>
2022-05-25 00:05:02 -0700

    Kitchen and Bar class added

---
+++ Chapeau2022/Model/Bar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Bar
+    {
+        public int TableNr { get; set; }
+        public DateTime Placed { get; set; }
+        public int Quantity { get; set; }
+        public string DrinkOrder { get; set; }
+        public Bar(int tableNr, DateTime placed, int quantity, string drinkOrder)
+        {
+            TableNr = tableNr;
+            Placed = placed;
+            Quantity = quantity;
+            DrinkOrder = drinkOrder;
+        }
+    }
+}
